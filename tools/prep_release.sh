fe676a73a6fc89022fb4ae0e342aa447ba3ec9a2
Alex Lowe <alex.lowe@canonical.com>
2023-11-16 20:41:03 -0500

    use astral and build from source

--- tools/prep_release.sh
+++ tools/prep_release.sh
@@ -4,13 +4,11 @@
 set -eu -o pipefail
 
 upstream_release=$1
-release_tag=$2
-release_file=$3
 
 echo "Preparing upstream release ${upstream_release}"
 
 echo "Setting snap version to ${upstream_release}"
 sed -i 's/^\(version: \).*$/\1'"${upstream_release}"'/' snap/snapcraft.yaml
 
-echo "Setting GitHub downloads to ${upstream_release}"
-sed -i "s|/download/v[0-9.]*/ruff|/download/${upstream_release}/ruff|" snap/snapcraft.yaml
+echo "Setting tags to ${upstream_release}"
+sed -i 's/^\( *source-tag: \).*$/\1'"${upstream_release}/" snap/snapcraft.yaml
