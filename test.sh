6fe0a124d654e2454e38f212c8f8cf6dab4b5112
Alex Lowe <alex.lowe@canonical.com>
2024-02-17 17:48:59 -0500

    test: make test summary a table

--- test.sh
+++ test.sh
@@ -9,7 +9,6 @@ cleanup(){
 trap cleanup EXIT
 
 summary_file="${GITHUB_STEP_SUMMARY:-/dev/stderr}"
-echo -e '## Summary:\n' > "$summary_file"
 
 repositories(){
     echo https://github.com/canonical/snapcraft
@@ -27,22 +26,26 @@ test_repo(){
     echo "::group::Repository: ${repo}"
     local repo_dir=$(mktemp --directory ruff-snap-test-XXXXXXXX)
     git clone --depth=1 $1 $repo_dir
-    check_result="Success"
-    if ruff check --exit-zero --select=ALL --preview --fix --unsafe-fixes "${repo_dir}" ; then
-        echo "- ${repo}: check success" >> $summary_file
+    echo -n "| ${repo} | $(cd $repo_dir; git rev-parse --short HEAD) | " >> $summary_file
+    if (cd "${repo_dir}"; ruff check); then
+        echo -n "✅ | " >> $summary_file
     else
-        exit_code=$?
-        any_failed=true
-        check_result="Failed"
-        echo "- ${repo}: Failed with exit code $exit_code" >> $summary_file
+        echo -n "❌ | " >> $summary_file
     fi
-    if ruff format --preview "${repo_dir}" ; then
-        echo "- ${repo}: format success" >> $summary_file
+    if (cd "${repo_dir}"; ruff check --preview --fix --unsafe-fixes); then
+        echo -n "✅ | " >> $summary_file
     else
-        exit_code=$?
-        any_failed=true
-        check_result="Failed"
-        echo "- ${repo}: Failed with exit code $exit_code" >> $summary_file
+        echo -n "❌ | " >> $summary_file
+    fi
+    if (cd "${repo_dir}"; ruff format); then
+        echo -n "✅ | " >> $summary_file
+    else
+        echo -n "❌ | " >> $summary_file
+    fi
+    if (cd "${repo_dir}"; ruff format --preview); then
+        echo "✅ | " >> $summary_file
+    else
+        echo "❌ | " >> $summary_file
     fi
 
     echo "::endgroup::"
@@ -59,12 +62,8 @@ echo "::group::Rules:"
 ruff rule --all
 echo "::endgroup::"
 
-any_failed=false
+echo "| Repository | Commit | Standard check | Check + preview + fix | Format | Format + preview |" >> $summary_file
+echo "|------------|--------|----------------|-----------------------|--------|------------------|" >> $summary_file
 for repo in $(repositories); do
     test_repo "${repo}"
 done
-if [[ $any_failed == "true" ]]; then
-    echo "Overall: Failed"
-    exit 1
-fi
-echo "Overall: Succeeded"
